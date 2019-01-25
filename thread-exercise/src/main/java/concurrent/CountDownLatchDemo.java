56d62dea5be9db235c57418aa0dd3942b6c3f95f
yfshen <596090969@qq.com>
2019-01-25 14:49:33 +0800

    代码更新

--- thread-exercise/src/main/java/concurrent/CountDownLatchDemo.java
+++ thread-exercise/src/main/java/concurrent/CountDownLatchDemo.java
@@ -9,27 +9,23 @@ import java.util.concurrent.Executors;
  * 栅栏：计数器
  * @author yfshen
  */
-public class CountDownLatchDemo implements Runnable {
+public class CountDownLatchDemo {
     static final CountDownLatch END = new CountDownLatch(10);
-    static final CountDownLatchDemo DEMO = new CountDownLatchDemo();
-
-    @Override
-    public void run() {
-        try {
-            // 模拟检查任务
-            Thread.sleep(new Random().nextInt(10) * 1000);
-            System.out.println("check complete");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            END.countDown();
-        }
-    }
 
     public static void main(String[] args) {
         ExecutorService exec = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 10; i++) {
-            exec.submit(DEMO);
+            exec.submit(() -> {
+                try {
+                    // 模拟检查任务
+                    Thread.sleep(new Random().nextInt(10) * 1000);
+                    System.out.println("check complete");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    END.countDown();
+                }
+            });
         }
         // 等待检查
         try {
